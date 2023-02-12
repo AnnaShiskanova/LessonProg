0f7624ebb1d9f01189513cdda25f9b01e8c2e370
Anna <shiskanovaag@mail.ru>
2023-02-12 17:44:53 +1000

    program for task 3

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// напишите программу, которая на вход принимает число и выдает, 
+// является  ли число четным (делится ли оно на 2 без остатка) 
+
+Console.WriteLine("Enter any number");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
