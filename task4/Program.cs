1fee3c36bb0061dec9c33f1b56cc7cc250b67ff9
Anna <shiskanovaag@mail.ru>
2023-02-12 18:16:29 +1000

    remember

---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿// напишете программу, которая на вход принимает число (N), 
+// а на выходе показывает нечетные числа  от  1 до N
+
+Console.WriteLine("Enter any number");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine(i);
+}
